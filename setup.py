69591c86ab847333ee32bfc7676bbc50ee7fcaac
Aarni Koskela <akx@iki.fi>
2016-08-29 17:45:34 +0300

    Become 0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-i18nkit',
-    version='0.0.1',
+    version='0.0.2',
     author='Aarni Koskela',
     author_email='akx@iki.fi',
     description='Internationalization utilities for Django.',
