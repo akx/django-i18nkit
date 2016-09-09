366471b0228f432fa23af5b4c1128b222cf768b6
Aarni Koskela <akx@iki.fi>
2016-09-09 09:11:23 +0300

    Late-import jinja2 (and adapt tests)

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -1,3 +1,4 @@
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext_lazy as _, ngettext_lazy as ngettext
 
 _('Ohai!')
+ngettext('I have %s monkey.', 'I have %s monkeys.', 13)
